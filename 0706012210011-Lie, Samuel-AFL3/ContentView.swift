4711cc99769f720d31cd065734209b746bd3aef9
AttraqtiA <samuelmiracle.lie@gmail.com>
2024-04-18 11:41:51 +0700

    Initial Commit

---
+++ 0706012210011-Lie, Samuel-AFL3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  0706012210011-Lie, Samuel-AFL3
+//
+//  Created by student on 18/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
