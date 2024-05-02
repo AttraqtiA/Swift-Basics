80b19d44340168c8e8f8f2ad0859d90152d1c5ae
AttraqtiA <samuelmiracle.lie@gmail.com>
2024-05-02 15:19:39 +0700

    Notification fix final

--- WatchLandmarks Watch App/NotificationView.swift
+++ WatchLandmarks Watch App/NotificationView.swift
@@ -12,7 +12,6 @@ struct NotificationView: View {
     var message: String?
     var landmark: Landmark?
 
-
     var body: some View {
         VStack {
             if let landmark {
@@ -24,9 +23,7 @@ struct NotificationView: View {
             Text(title ?? "Unknown Landmark")
                 .font(.headline)
 
-
             Divider()
-
 
             Text(message ?? "You are within 5 miles of one of your favorite landmarks.")
                 .font(.caption)
